f35faed6092e9c83a1c551a1e00451ad0f870bed
AdeshApradh <53626187+AdeshApradh@users.noreply.github.com>
2021-02-24 23:09:21 +0530

    Initialize dev

---
+++ test/insertGame.test.js
@@ -0,0 +1,17 @@
+const insertGame = require("../functions/insertGame");
+const faker = require("faker");
+
+const gameId = faker.helpers.replaceSymbols("????");
+
+it.skip("should create a game document", async () => {
+  const response = await insertGame.handler({
+    path: "/functions/insertGame/" + gameId,
+    body: '{"user":"me"}',
+  });
+  expect(response.statusCode).toBe(200);
+});
+
+it.skip("shouldn't create a game document without a game id", async () => {
+  const response = await insertGame.handler({ path: "insertGame" });
+  expect(response.statusCode).toBe(400);
+});
